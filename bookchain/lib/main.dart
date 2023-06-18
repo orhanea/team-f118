d8b55964379e958b71f4f7962e775be6837cc42d
Orhan Emre Anıl <44325686+orhanea@users.noreply.github.com>
2023-06-18 23:26:02 +0300

    small changes

--- bookchain/lib/main.dart
+++ bookchain/lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:bookchain/meta_app/screens/homePage.dart';
-import 'meta_app/screens/welcomeScreens.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:bookchain/meta_app/screens/onbarding.dart';
+
+// ignore: unused_import
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,36 +13,33 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: GoogleFonts.urbanistTextTheme(),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        useMaterial3: true,
       ),
       home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
+        stream: null,
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const HomePage();
-          } else {
-            return const WelcomeScreen();
-          }
-        },
+          return AnimatedSplashScreen(
+            splash: const Image(
+              image: AssetImage('assets/images/ouap.png'),
+              height: 300.0,
+              width: 300.0,
+            ),
+            duration: 3000,
+            splashTransition: SplashTransition.fadeTransition,
+            backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+            nextScreen: const OnBoarding(),
+          );
+        }
       ),
     );
   }

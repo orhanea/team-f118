f8a180fa0f6e09e15e66d82c3fd0d00de5bc9e28
Can Zunal <canzunal@sabanciuniv.edu>
2023-07-02 14:08:05 +0300

    Added validation to goal creation page

--- bookchain/lib/meta_app/screens/createNewGoal.dart
+++ bookchain/lib/meta_app/screens/createNewGoal.dart
@@ -15,6 +15,9 @@ class _CreateNewGoalState extends State<CreateNewGoal> {
   late User? currentUser;
   String? userName;
   String? selectedFrequency;
+  DateTime? selectedDate;
+
+  final _formKey = GlobalKey<FormState>(); // Create a global form key
 
   @override
   void initState() {
@@ -32,6 +35,22 @@ class _CreateNewGoalState extends State<CreateNewGoal> {
     }
   }
 
+  Future<void> selectDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,221 +58,257 @@ class _CreateNewGoalState extends State<CreateNewGoal> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Create New Goal',
-                style: GoogleFonts.inter(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 15,
-                ),
-              ),
-              SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        labelStyle: GoogleFonts.inter(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 14,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                        left: 25.0), // Adjust the padding as needed
-                    child: Icon(
-                      Icons.landscape,
-                      color: Colors.blue,
-                      size: 20.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Date',
-                        labelStyle: GoogleFonts.inter(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 14,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                        left: 25.0), // Adjust the padding as needed
-                    child: Icon(
-                      Icons.calendar_today_rounded,
-                      color: Colors.blue,
-                      size: 20.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 12),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Add Task',
-                  labelStyle: GoogleFonts.inter(
-                    fontWeight: FontWeight.w300,
+          child: Form(
+            key: _formKey, // Assign the form key to the Form widget
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Create New Goal',
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w700,
                     fontSize: 15,
                   ),
-                  prefixIcon: Icon(
-                    Icons.add_circle,
-                    color: Colors.grey,
-                    size: 20.0,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
                 ),
-              ),
-              SizedBox(height: 12),
-              DropdownButtonFormField<String>(
-                value: selectedFrequency,
-                onChanged: (newValue) {
-                  setState(() {
-                    selectedFrequency = newValue;
-                  });
-                },
-                items: [
-                  DropdownMenuItem(
-                    value: 'Daily',
-                    child: Text(
-                      'Daily',
-                      style: GoogleFonts.inter(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 15,
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          labelStyle: GoogleFonts.inter(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 14,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a name';
+                          }
+                          return null;
+                        },
                       ),
                     ),
-                  ),
-                  DropdownMenuItem(
-                    value: 'Weekly',
-                    child: Text(
-                      'Weekly',
-                      style: GoogleFonts.inter(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 15,
+                    const Padding(
+                      padding: EdgeInsets.only(
+                          left: 25.0), // Adjust the padding as needed
+                      child: Icon(
+                        Icons.landscape,
+                        color: Colors.blue,
+                        size: 20.0,
                       ),
                     ),
-                  ),
-                  DropdownMenuItem(
-                    value: 'Monthly',
-                    child: Text(
-                      'Monthly',
-                      style: GoogleFonts.inter(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ],
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    flex: 2,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Start time',
-                        labelStyle: GoogleFonts.inter(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12,
-                        ),
-                        prefixIcon: Icon(Icons.access_time),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 8), // Add a small gap
-                  Flexible(
-                    flex: 2,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'End time',
-                        labelStyle: GoogleFonts.inter(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 12,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.access_time,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          selectDate(context);
+                        },
+                        child: AbsorbPointer(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Date',
+                              labelStyle: GoogleFonts.inter(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 14,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a date';
+                              }
+                              return null;
+                            },
+                            controller: TextEditingController(
+                              // Set the text field value to the selected date
+                              text: selectedDate != null
+                                  ? selectedDate!.toString().split(' ')[0]
+                                  : '',
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Tap to add notes',
-                  labelStyle: GoogleFonts.inter(
-                    fontWeight: FontWeight.w300,
-                    fontSize: 15,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  suffixIcon: Icon(Icons.edit),
+                    const Padding(
+                      padding: EdgeInsets.only(
+                          left: 25.0), // Adjust the padding as needed
+                      child: Icon(
+                        Icons.calendar_today_rounded,
+                        color: Colors.blue,
+                        size: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle create goal button press
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  backgroundColor: Colors.blue,
-                ),
-                child: Center(
-                  child: Text(
-                    'Create Goals',
-                    style: GoogleFonts.inter(
+                SizedBox(height: 12),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Add Task',
+                    labelStyle: GoogleFonts.inter(
                       fontWeight: FontWeight.w300,
-                      fontSize: 17,
-                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.add_circle,
+                      color: Colors.grey,
+                      size: 20.0,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 16),
-            ],
+                SizedBox(height: 12),
+                DropdownButtonFormField<String>(
+                  value: selectedFrequency ??
+                      'Daily', // Set the desired initial value
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedFrequency = newValue;
+                    });
+                  },
+                  items: [
+                    DropdownMenuItem(
+                      value: 'Daily',
+                      child: Text(
+                        'Daily',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    DropdownMenuItem(
+                      value: 'Weekly',
+                      child: Text(
+                        'Weekly',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    DropdownMenuItem(
+                      value: 'Monthly',
+                      child: Text(
+                        'Monthly',
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ],
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Start time',
+                          labelStyle: GoogleFonts.inter(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 12,
+                          ),
+                          prefixIcon: Icon(Icons.access_time),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 8), // Add a small gap
+                    Flexible(
+                      flex: 2,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'End time',
+                          labelStyle: GoogleFonts.inter(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 12,
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.access_time,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Tap to add notes',
+                    labelStyle: GoogleFonts.inter(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 15,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    suffixIcon: Icon(Icons.edit),
+                  ),
+                ),
+                SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // The form is valid, proceed with creating the goal
+                      createGoal();
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Create Goals',
+                      style: GoogleFonts.inter(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 17,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void createGoal() {
+    // Handle create goal button press
   }
 }

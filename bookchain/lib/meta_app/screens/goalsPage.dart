02c99bb15d860fb18b2795f45dbe8b688a2a8c34
Can Zunal <canzunal@sabanciuniv.edu>
2023-07-02 11:57:35 +0300

    Goals page calendar added (design improvment needed)

--- bookchain/lib/meta_app/screens/goalsPage.dart
+++ bookchain/lib/meta_app/screens/goalsPage.dart
@@ -1,20 +1,75 @@
 import 'package:flutter/material.dart';
-import 'createNewGoal.dart';
-import 'homePage.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class GoalsPage extends StatelessWidget {
+class GoalsPage extends StatefulWidget {
+  @override
+  _GoalsPageState createState() => _GoalsPageState();
+}
+
+class _GoalsPageState extends State<GoalsPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime _selectedDay = DateTime.now();
+
+  Map<DateTime, List<dynamic>> _events = {
+    DateTime.now(): [
+      'Event 1',
+      'Event 2',
+    ],
+  };
+
+  List<dynamic> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Goals'),
-        automaticallyImplyLeading: false, // Prevents the back arrow buttons
       ),
-      body: Center(
-        child: Text(
-          'Goals Page',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'Now, today',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 16.0),
+          Expanded(
+            child: TableCalendar(
+              calendarFormat: _calendarFormat,
+              focusedDay: _focusedDay,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              eventLoader: _getEventsForDay,
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+          ),
+        ],
       ),
     );
   }

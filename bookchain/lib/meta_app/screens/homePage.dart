5129309fbaa06c844c4fc80b2c857b5de4a8e4db
Orhan Emre Anıl <44325686+orhanea@users.noreply.github.com>
2023-07-03 10:00:00 +0300

    Update homePage.dart

--- bookchain/lib/meta_app/screens/homePage.dart
+++ bookchain/lib/meta_app/screens/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -22,9 +23,11 @@ class _HomePageState extends State<HomePage> {
   Future<void> fetchUserName() async {
     if (currentUser != null) {
       await currentUser!.reload();
-      setState(() {
-        userName = currentUser!.displayName;
-      });
+      setState(
+        () {
+          userName = currentUser!.displayName;
+        },
+      );
     }
   }
 
@@ -34,18 +37,63 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Homepage"),
       ),
-      body: Column(
-        children: [
-          Text("Hello ${userName ?? ''}"),
-          ElevatedButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacementNamed(context, '/welcome');
-            },
-            child: const Text("Sign Out"),
-          ),
-        ],
-      ),
-    );
-  }
+      body: Center(
+        child: Container(
+            color: Colors.grey[100],
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircularPercentIndicator(
+                  radius: 120,
+                  lineWidth: 30,
+                  backgroundColor: Colors.grey,
+                  progressColor: Colors.red,
+                  percent: 0.6,
+                  center: Text(
+                    "60%",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  circularStrokeCap: CircularStrokeCap.round,
+                  animation: true,
+                  animationDuration: 2000,
+                  onAnimationEnd: () {
+                    print("Animation finished");
+                  },
+                  header: Text(
+                    "Task Progress",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                LinearPercentIndicator(
+                  width: 180,
+                  lineHeight: 30,
+                  backgroundColor: Colors.grey,
+                  progressColor: Colors.blue,
+                  percent: 0.8,
+                  center: Text(
+                    "80%",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  alignment: MainAxisAlignment.center,
+                  animation: true,
+                  animationDuration: 1000,
+                  onAnimationEnd: () {
+                    print("Linear Animation finished");
+                  },
+                  barRadius: Radius.circular(20),
+                  leading: Text(
+                    "Task Progress",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
 }

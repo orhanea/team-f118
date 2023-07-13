6d2657888fbc155a5959286d409fbf74a8d8d31e
Orhan Emre Anıl <44325686+orhanea@users.noreply.github.com>
2023-07-13 21:50:19 +0300

    Toggle button is added

--- bookchain/lib/meta_app/screens/goalsPage.dart
+++ bookchain/lib/meta_app/screens/goalsPage.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum GoalsFilter {
+  all,
+  completed,
+}
+
 class GoalsPage extends StatefulWidget {
   @override
   _GoalsPageState createState() => _GoalsPageState();
@@ -15,6 +20,7 @@ class _GoalsPageState extends State<GoalsPage> {
   Map<DateTime, List<dynamic>> _events = {};
   late String currentUserUid;
   int completedGoals = 0;
+  GoalsFilter _goalsFilter = GoalsFilter.all;
 
   Future<void> _fetchEvents() async {
     String currentUserUid = FirebaseAuth.instance.currentUser!.uid;
@@ -30,7 +36,7 @@ class _GoalsPageState extends State<GoalsPage> {
       completedGoals = 0; // Reset the completed goals count
       for (QueryDocumentSnapshot goalDoc in goalsSnapshot.docs) {
         Map<String, dynamic>? goalData =
-            goalDoc.data() as Map<String, dynamic>?;
+        goalDoc.data() as Map<String, dynamic>?;
 
         if (goalData != null) {
           DateTime goalDate = (goalData['date'] as Timestamp).toDate();
@@ -41,7 +47,7 @@ class _GoalsPageState extends State<GoalsPage> {
           } else {
             _events[goalDate] = [goalName];
           }
-          if (goalData != null && goalData['completed']) {
+          if (goalData['completed'] == true) {
             completedGoals++;
           }
         }
@@ -56,8 +62,7 @@ class _GoalsPageState extends State<GoalsPage> {
   @override
   void initState() {
     super.initState();
-    currentUserUid =
-        FirebaseAuth.instance.currentUser!.uid; // Assign the value here
+    currentUserUid = FirebaseAuth.instance.currentUser!.uid;
     _fetchEvents();
   }
 
@@ -65,18 +70,18 @@ class _GoalsPageState extends State<GoalsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Goals'),
+        title: const Text('Goals'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 16.0),
+          const SizedBox(height: 16.0),
           Expanded(
             child: Column(
               children: [
                 Card(
                   elevation: 4.0,
-                  margin: EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.all(16.0),
                   child: TableCalendar(
                     calendarFormat: _calendarFormat,
                     focusedDay: _focusedDay,
@@ -102,7 +107,7 @@ class _GoalsPageState extends State<GoalsPage> {
                     },
                   ),
                 ),
-                SizedBox(height: 16.0),
+                const SizedBox(height: 16.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -112,48 +117,69 @@ class _GoalsPageState extends State<GoalsPage> {
                         _selectedDay == null
                             ? 'Goals for the user'
                             : 'Goals for ${_selectedDay?.day}-${_selectedDay?.month}-${_selectedDay?.year}',
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    SizedBox(height: 8.0),
-                    ElevatedButton(
-                      onPressed: () {
+                    const SizedBox(height: 8.0),
+                    ToggleButtons(
+                      isSelected: [
+                        _goalsFilter == GoalsFilter.all,
+                        _goalsFilter == GoalsFilter.completed,
+                      ],
+                      onPressed: (index) {
                         setState(() {
                           _selectedDay = null;
+                          _goalsFilter =
+                          GoalsFilter.values[index];
                         });
                       },
-                      child: Text('Show All Goals'),
+                      children: const [
+                        Icon(Icons.list),
+                        Icon(Icons.check),
+                      ],
                     ),
                   ],
                 ),
-                SizedBox(height: 16.0),
+                const SizedBox(height: 16.0),
                 Expanded(
                   child: StreamBuilder<QuerySnapshot>(
                     stream: _selectedDay == null
+                        ? _goalsFilter == GoalsFilter.all
                         ? FirebaseFirestore.instance
-                            .collection('users')
-                            .doc(currentUserUid)
-                            .collection('goals')
-                            .snapshots()
+                        .collection('users')
+                        .doc(currentUserUid)
+                        .collection('goals')
+                        .snapshots()
                         : FirebaseFirestore.instance
-                            .collection('users')
-                            .doc(currentUserUid)
-                            .collection('goals')
-                            .where('date',
-                                isEqualTo: Timestamp.fromDate(_selectedDay!))
-                            .snapshots(),
+                        .collection('users')
+                        .doc(currentUserUid)
+                        .collection('goals')
+                        .where('completed', isEqualTo: true)
+                        .snapshots()
+                        : FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(currentUserUid)
+                        .collection('goals')
+                        .where('date',
+                        isEqualTo: Timestamp.fromDate(_selectedDay!))
+                        .snapshots(),
                     builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Center(child: CircularProgressIndicator());
+                      if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const Center(
+                            child: CircularProgressIndicator());
                       }
 
-                      if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      if (!snapshot.hasData ||
+                          snapshot.data!.docs.isEmpty) {
                         return Center(
                           child: Text(
-                            'No goals found for $_selectedDay',
+                            _goalsFilter == GoalsFilter.all
+                                ? 'No goals found for $_selectedDay'
+                                : 'No completed goals found for $_selectedDay',
                             style: TextStyle(
                               fontSize: 16.0,
                               color: Colors.grey[700],
@@ -162,12 +188,15 @@ class _GoalsPageState extends State<GoalsPage> {
                         );
                       }
                       return ListView(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8.0),
                         children: snapshot.data!.docs.map((goalDoc) {
-                          
-                          final goalData = goalDoc.data() as Map<String, dynamic>;
+                          final goalData =
+                          goalDoc.data() as Map<String, dynamic>;
                           final goalId = goalDoc.id;
-                          bool isCompleted = goalData['completed'] ?? false;
+
+                          bool isCompleted =
+                              goalData['completed'] ?? false;
 
                           return Card(
                             elevation: 2.0,
@@ -195,9 +224,9 @@ class _GoalsPageState extends State<GoalsPage> {
                                     setState(() {
                                       isCompleted = value ?? false;
                                       if (isCompleted) {
-                                        completedGoals++; // Increment completed goals count
+                                        completedGoals++;
                                       } else {
-                                        completedGoals--; // Decrement completed goals count
+                                        completedGoals--;
                                       }
                                     });
                                   }).catchError((error) {
